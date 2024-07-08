6214ace2d50c2c38565e370cdbf75d5dcd876296
Daniel <danielmelaku1177@gmail.com>
2024-07-08 02:55:43 -1200

    first commit ui design react

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,37 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Your Dream Home Awaits</h1>
+          <p>
+            Discover your ideal home with us. Our extensive listings and expert guidance ensure that you'll find a property that perfectly suits your needs and lifestyle. With a commitment to excellence, we provide unparalleled service and support throughout your real estate journey.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Properties Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="Background" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
